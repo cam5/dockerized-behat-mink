fd962e81149e30340c2940364746344deee1d007
Cameron Hurd <me@cameronhurd.com>
2018-10-13 13:50:08 -0400

    try to abstract it out

---
+++ ci/prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ ! -f var/cache/prod ]]; then
+ mkdir -p var/cache/prod
+fi;
+
+chmod g+w var/cache/prod && chmod g+x var/cache/prod
+
+if [[ ! -f var/logs/prod ]]; then
+ mkdir -p var/logs/prod
+fi;
+
+chmod g+w var/logs/prod
+
+if [[ ! -f var/sessions/prod ]]; then
+ mkdir -p var/sessions/prod
+fi;
+
+chmod g+w var/sessions/prod
